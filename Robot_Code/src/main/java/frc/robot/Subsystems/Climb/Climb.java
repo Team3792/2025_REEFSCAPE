38ecc377a2a08e19e82e8dcc7bd15f55227da9a1
keatonshall <keatonshall314@gmail.com>
2025-03-02 11:28:52 -0600

    [Evan] Climb configuration and voltage command

--- Robot_Code/src/main/java/frc/robot/Subsystems/Climb/Climb.java
+++ Robot_Code/src/main/java/frc/robot/Subsystems/Climb/Climb.java
@@ -26,17 +26,16 @@ public class Climb extends SubsystemBase {
   public Climb() {
     //Motor configuration
     
-    TalonFXConfiguration climbMotorConfiguration = new TalonFXConfiguration();
-    climbMotorConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    TalonFXConfiguration config = ClimbConstants.getConfig();
 
-    climbMotorConfiguration.Slot0.kG = ClimbConstants.kG;
-    climbMotorConfiguration.Slot0.kP = ClimbConstants.kP;
-    climbMotorConfiguration.Slot0.kI = ClimbConstants.kI;
-    climbMotorConfiguration.Slot0.kD = ClimbConstants.kD;
+    leftMotor.getConfigurator().apply(config);
+    rightMotor.getConfigurator().apply(config);
+    zeroPosition();
+  }
 
-    leftMotor.getConfigurator().apply(climbMotorConfiguration);
-    rightMotor.getConfigurator().apply(climbMotorConfiguration);
-    
+  public void zeroPosition(){
+    leftMotor.setPosition(0);
+    rightMotor.setPosition(0);
   }
 
   public void setPosition(double leftDegrees, double rightDegrees){
@@ -51,7 +50,7 @@ public class Climb extends SubsystemBase {
     rightMotor.setVoltage(rightVoltage);
   }
 
-  public Command voltageClimbCommandFactory(double voltage){
+  public Command voltageClimbCommand(double voltage){
     return Commands.startEnd(
       () -> {applyVoltage(voltage, voltage);}, 
       () -> {applyVoltage(0, 0);},
